49d2c095e64c4c2e0d656191dd3393a9226c6e01
Nadezhda Saunanen <saunanen@yandex.ru>
2023-07-06 02:19:51 +0300

    Решение дополнительной задачи

---
+++ Example_from_C#400/Program.cs
@@ -0,0 +1,14 @@
+﻿// Вывести факториал числа n, где гарантируется, что n не больше 10.
+
+Console.WriteLine("Введите целое число от 1 до 10: ");
+int n = int.Parse(Console.ReadLine());
+
+if (n > 0 && n <= 10)
+{
+    int factorial = 1;
+    for (int i = 1; i <= n; i++) factorial *= i;
+    Console.WriteLine($"Факториал числа: {n} = {factorial}");
+} else
+{
+    Console.WriteLine("Введенное число некорректно");
+}
